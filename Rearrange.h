0efa52ca1399bfc560fb93555e2ddb880f3d7254
StirringWizard1 <100811927+StirringWizard1@users.noreply.github.com>
2022-04-29 21:56:45 +0930

    Add files via upload

---
+++ Rearrange.h
@@ -0,0 +1,19 @@
+#ifndef REARRANGE_H
+#define REARRANGE_H
+
+
+#include "Mutator.h"
+#include "Individual.h"
+
+class Rearrange: public Mutator 
+{
+protected: 
+private:  
+public: 
+
+
+
+	virtual Individual * Mutate(Individual * In, int k);
+
+};
+#endif 
